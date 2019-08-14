9a3787c6b2424eea1508415c5318fdbb9ec858fe
rahibamin <54030063+rahibamin@users.noreply.github.com>
2019-08-14 14:50:37 -0400

    Add files via upload

---
+++ implicitPromotion.java
@@ -0,0 +1,15 @@
+package Session4.Assignment2;
+
+public class implicitPromotion {
+
+	public static void main(String[] args) {
+	
+		int x = 5;
+		double y = 6.9;
+		
+		y = y/x;
+		System.out.println(y);
+		
+	}
+
+}
